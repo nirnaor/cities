09e2b8aa0fbec444d5e64884d8b918bcd323b9e1
Nir Naor <nir@win.com>
2013-05-04 12:20:08 +0300

    Added validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,6 @@
 class City < ActiveRecord::Base
   attr_accessible :costs, :education, :name, :pollution, :security, :transportation
+  validates :name, :presence => true
+  validates :costs, :education, :pollution, :security,
+    :inclusion => { :in => 1..10, :message => "value must be betwene 1 and 5" } 
 end
