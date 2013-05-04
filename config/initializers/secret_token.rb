6cf54d66f89a588005b98b1d5c33ce32e992b2f4
Nir Naor <nir@win.com>
2013-05-04 11:46:37 +0300

    first commit of fresh project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cities::Application.config.secret_token = '41492f967746758563be100adb270df1a8c6fc26e1cbd03d73f7261c8df9d6473d112b701937d01d19edb081bdd2496e9206ee0f50fbba36225e151934037afb'
