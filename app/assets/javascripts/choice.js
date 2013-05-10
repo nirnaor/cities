3dc34118c937dce2b2950fada795b679144e90d6
Nir Naor <nir@win.com>
2013-05-10 10:49:23 +0300

    Summing up the inputs.

--- app/assets/javascripts/choice.js
+++ app/assets/javascripts/choice.js
@@ -1,6 +1,20 @@
 $(document).ready( function(){
-  $( "#sortable" ).sortable();
-  $( "#sortable" ).disableSelection();
+  console.log( "bitch" );
+  var num_inputs = "input[type='number']";
+
+
+  $(num_inputs).change( function( ev ) {
+    var sum = 0;
+
+    _( $(num_inputs) ).each( function( item ) {
+      console.log( item.value );
+      sum += parseInt( item.value || 0 );
+    } );
+
+    console.log( sum );
+  } );
+
+
 } ) ;
 
 
