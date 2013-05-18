e1ccc25e439207ff48fc2186676a3b28c850c1d5
Nir Naor <nir@win.com>
2013-05-18 19:19:44 +0300

    Added a method city_grade that calculates grade
    for each city.

--- app/controllers/choice_controller.rb
+++ app/controllers/choice_controller.rb
@@ -1,5 +1,22 @@
 class ChoiceController < ApplicationController
   def check
     @city = City.new
+
+    City.all.each do | city |
+      puts city['name'], city_grade( city )
+    end
+    
+  end
+
+
+  def city_grade( city )
+    if params[:city] != nil  
+      result = 0
+      params[:city].keys.each do |pr| 
+        result += Integer(city[pr]) * Integer(params["city"][pr]) 
+      end
+      result
+    end
+
   end
 end
