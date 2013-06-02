eafb5122530be617f32098b1d8e8c02c4d0eae7f
Nir Naor <nir@win.com>
2013-06-02 08:55:33 +0300

    using the original values

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,5 @@ class City < ActiveRecord::Base
   attr_accessible :costs, :education, :name, :enviornment, :security, :transportation
   validates :name, :presence => true
   validates :costs, :education, :enviornment, :security,
-    :inclusion => { :in => 1..20, :message => "value must be betwene 1 and 20" } 
+    :inclusion => { :in => 0..100, :message => "value must be betwene 1 and 20" } 
 end
